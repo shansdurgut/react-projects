fab54ed02bb9e70f0d36af90bc71e207ca3c3b81
Shan Shan Huang <91642948+shansdurgut@users.noreply.github.com>
2022-05-10 11:07:53 -0500

    added pokecard and pokedex

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,14 +1,13 @@
 import {Component} from "react";
-import PokeCard from "./pokecard.component/pokecard.component";
-import './App.css';
+
+import Pokedex from "./pokedex.component/pokedex.component";
 
 class App extends Component{
     render(){
       return(
           <div>
-            <PokeCard id={4} name={"Charmander"} type={"fire"} exp={62 }>
-
-            </PokeCard>
+              <Pokedex>
+              </Pokedex>
           </div>
           )
     }
--- pokemon/src/pokecard.component/pokecard.component.jsx
+++ pokemon/src/pokecard.component/pokecard.component.jsx
@@ -1,12 +1,17 @@
 import {Component} from "react";
+import "./pokecard.component.scss";
+
+const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class PokeCard extends Component{
     render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`
         return (
             <div className={"PokeCard"}>
                 <h1>{this.props.name}</h1>
-                <h1>{this.props.type}</h1>
-                <h1>{this.props.exp}</h1>
+                <img src={imgSrc} alt={this.props.name}/>
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.exp}</div>
             </div>
         )
     }
