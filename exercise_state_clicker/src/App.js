1f57ada18779a8553a6d1870fa4bc1bde7f3ab00
Shan Shan Huang <91642948+shansdurgut@users.noreply.github.com>
2022-05-11 16:22:26 -0500

    using this.state and setState to get random number when clicked!

---
+++ exercise_state_clicker/src/App.js
@@ -0,0 +1,29 @@
+import {Component} from "react";
+import './App.css';
+import "./clicker"
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {num:0};
+  }
+
+  handleClick = () => {
+    let rand =Math.floor(Math.random()*10);
+    this.setState( {num : rand});
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>Number is: {this.state.num}</h1>
+         <button onClick={this.handleClick}>Click and get random number!</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
